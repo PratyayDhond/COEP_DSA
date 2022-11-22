d41054c3ed8aba0f22d256730d6e93df50983e75
Pratyay Dhond <dhondpratyay@gmail.com>
2022-11-22 14:39:14 +0530

    6.c

---
+++ 6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+	int n =  atoi(argv[1]);
+	char c = '*';
+	
+	for(int i = 1; i <= n; i++ ){
+		
+		for(int j = 1; j <= n - i; j++){
+			printf("  ");
+		}
+		
+		for(int j = 1; j < 2*i; j++){
+			
+			printf("%c",c);							
+			printf(" ");
+		}
+		printf("\n");
+	}
+	
+	return 0;	
+}
